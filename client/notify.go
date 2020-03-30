2a53e35778ee13df398f393f20ae9c2f5d02df2e
Akvicor <akvicor@gmail.com>
2020-03-30 10:52:44 +0800

    1.0

--- client/notify.go
+++ client/notify.go
@@ -10,7 +10,7 @@ import (
 
 // [ip] [port] [uid] [key]
 func main() {
-	fmt.Println("IP:", os.Args[1], "PORT:", os.Args[2], "UID:", os.Args[3], time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println("IP:", os.Args[1], "PORT:", os.Args[2], "UID:", os.Args[3], time.Now().Format("2006-01-02 15:04:05"), "PID:", os.Getpid())
 	go tcp.ListenTCP(fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]), os.Args[3], os.Args[4]) // ip port uid
 	select {}
 }
